be1b484ac0b0c93089b375c1c83c5bf5581cc50c
Rich Rines <rich.rines@me.com>
2014-01-12 23:01:08 -0800

    more work on client

--- app/models/btce_gateway.rb
+++ app/models/btce_gateway.rb
@@ -1,2 +1,38 @@
+require 'btce'
+
 class BtceGateway
+  def btce_pull
+    instantiate_ticker
+
+    BitcoinPrice.create!(
+      avg: @ticker.avg,
+      buy: @ticker.buy,
+      high: @ticker.high,
+      last: @ticker.last,
+      low: @ticker.low,
+      server_time: @ticker.updated,
+      vol: @ticker.vol,
+      vol_cur: @ticker.vol_cur
+    )
+  end
+
+  private
+
+  def instantiate_ticker
+    @ticker ||= Btce::Ticker.new "btc_usd"
+  end
+
+  def first_derivative
+  end
+
+  def second_derivative
+  end
+
+  def five_period_moving_average
+    last_five = BitcoinPrice.find(:all, order: "created_at desc", limit: 5)
+  end
+
+  def ten_period_moving_average
+    last_ten = BitcoinPrice.find(:all, order: "created_at desc", limit: 10)
+  end
 end
--- spec/models/btce_gateway_spec.rb
+++ spec/models/btce_gateway_spec.rb
@@ -1,5 +1,21 @@
 require 'spec_helper'
 
-describe BtceGateway, '#method_name' do
+describe BtceGateway, '#btce_pull' do
+  pending
+end
+
+describe BtceGateway, '#btce_first_derivative' do
+  pending
+end
+
+describe BtceGateway, '#btce_second_derivative' do
+  pending
+end
+
+describe BtceGateway, '#five_period_moving_average' do
+  pending
+end
+
+describe BtceGateway, '#ten_period_moving_average' do
   pending
 end
